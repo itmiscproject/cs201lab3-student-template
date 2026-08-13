fbf6be85c0041071fcb929a9c6d2d9f578b05c95
cookiemo729 <58022041+cookiemo729@users.noreply.github.com>
2026-08-13 11:02:09 +0800

    first commit

---
+++ Q1Test.java
@@ -0,0 +1,69 @@
+public class Q1Test {
+     public static void main(String[] args) {
+
+          String result, traversal1, traversal2, traversal3;
+
+          traversal1 = "1-2";
+          traversal2 = "2-1-3";
+          traversal3 = "1-3-2";
+          result = verify(traversal1, traversal2, traversal3);
+          System.out.println("Traversal 1 : " + traversal1);
+          System.out.println("Traversal 2 : " + traversal2);
+          System.out.println("Traversal 3 : " + traversal3);
+          System.out.println("Expected Result : Invalid traversals");
+          System.out.println("  Actual Result : " + result);
+          System.out.println();
+
+          traversal1 = "1-2-3";
+          traversal2 = "2-1-3";
+          traversal3 = "1-3-2";
+          result = verify(traversal1, traversal2, traversal3);
+          System.out.println("Traversal 1 : " + traversal1);
+          System.out.println("Traversal 2 : " + traversal2);
+          System.out.println("Traversal 3 : " + traversal3);
+          System.out.println(
+                    "Expected Result : Traversal 1 - Inorder, Traversal 2 - Preorder, Traversal 3 - Postorder");
+          System.out.println("  Actual Result : " + result);
+          System.out.println();
+
+          traversal1 = "1-2-3";
+          traversal2 = "3-2-1";
+          traversal3 = "2-3-1";
+          result = verify(traversal1, traversal2, traversal3);
+          System.out.println("Traversal 1 : " + traversal1);
+          System.out.println("Traversal 2 : " + traversal2);
+          System.out.println("Traversal 3 : " + traversal3);
+          System.out.println("Expected Result : Invalid traversals");
+          System.out.println("  Actual Result : " + result);
+          System.out.println();
+
+          traversal1 = "3-1-2-5-4";
+          traversal2 = "2-1-4-5-3";
+          traversal3 = "1-2-3-4-5";
+          result = verify(traversal1, traversal2, traversal3);
+          System.out.println("Traversal 1 : " + traversal1);
+          System.out.println("Traversal 2 : " + traversal2);
+          System.out.println("Traversal 3 : " + traversal3);
+          System.out.println(
+                    "Expected Result : Traversal 1 - Preorder, Traversal 2 - Postorder, Traversal 3 - Inorder");
+          System.out.println("  Actual Result : " + result);
+          System.out.println();
+
+          traversal1 = "10-20-30-40-50";
+          traversal2 = "20-10-30-40-50";
+          traversal3 = "10-50-40-30-20";
+          result = verify(traversal1, traversal2, traversal3);
+          System.out.println("Traversal 1 : " + traversal1);
+          System.out.println("Traversal 2 : " + traversal2);
+          System.out.println("Traversal 3 : " + traversal3);
+          System.out.println(
+                    "Expected Result : Traversal 1 - Inorder, Traversal 2 - Preorder, Traversal 3 - Postorder");
+          System.out.println("  Actual Result : " + result);
+          System.out.println();
+     } 
+
+      // write your codes here
+     public static String verify(String traversal1, String traversal2, String traversal3){
+          return "";
+     }
+}
